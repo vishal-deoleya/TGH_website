a29a5af3eaf5497edb24d5e8d66e7ce03bcf0767
Vishal Singh Deoleya <deoleya009@gmail.com>
2018-07-25 01:11:52 +0530

    user scheme bug fixed

--- server.js
+++ server.js
@@ -2,15 +2,35 @@ var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
+var bodyParser = require('body-parser');
+var User = require('./models/user');
+var ejs = require('ejs');
+var ejsmate = require('ejs-mate');
+
 var app =express();
 mongoose.connect('mongodb://localhost:27017',function(err){
     if(err) return console.log('error');
     console.log('connected to database');
 });
-//middleware for morgan
+//middleware
 app.use(morgan('dev'));
-app.get('/',function(req,res){
-    res.json('imainnininia');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.engine('ejs',ejsmate);
+app.set('view engine','ejs');
+//routes
+app.get('/views',function(req,res){
+    res.render('home');
+});
+
+app.post('/createuser',function(req,res,next){
+    var user = new User();
+    user.profile.name = req.body.name;
+    user.password = req.password;
+    user.save(function(err){
+        if(err) return next(err);
+        res.json('data saved');
+    });
 });
 
 app.listen(3000,function(err){
